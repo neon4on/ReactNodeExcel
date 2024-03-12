dfd9737a1efdbb92fbf11af798e3a2c24a780a9f
neon4on <vkam37@gmail.com>
2024-03-12 20:14:07 +0400

    update app

--- backend/excelRoutes.js
+++ backend/excelRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const exceljs = require('exceljs');
+const xlsxPopulate = require('xlsx-populate');
 
 router.post('/createExcel51', (req, res) => {
   const tableData = req.body;
@@ -66,32 +67,147 @@ router.post('/createExcel51', (req, res) => {
       res.status(500).send('Error saving file');
     });
 });
+function columnIndexToName(index) {
+  let name = '';
+  while (index > 0) {
+    const remainder = (index - 1) % 26;
+    name = String.fromCharCode(65 + remainder) + name;
+    index = Math.floor((index - remainder) / 26);
+  }
+  return name;
+}
 
-router.post('/createExcel54', (req, res) => {
+function columnNameToIndex(name) {
+  let index = 0;
+  for (let i = 0; i < name.length; i++) {
+    index = index * 26 + name.charCodeAt(i) - 64;
+  }
+  return index;
+}
+
+router.post('/createExcel54', async (req, res) => {
+  const tableData = req.body;
+
+  async function insertDataAfterRow(filePath, sheetName, columnAValue, columnBValue) {
+    try {
+      const workbook = await xlsxPopulate.fromFileAsync(filePath);
+      const sheet = workbook.sheet(sheetName);
+
+      // Находим индекс строки по значению в столбце A
+      let rowIndex = null;
+      let columnIndexB = null;
+      sheet
+        .usedRange()
+        .value()
+        .forEach((row, index) => {
+          if (row[0] === columnAValue) {
+            rowIndex = index;
+            console.log(`Значение ${columnAValue} найдено в столбце A.`);
+          }
+          if (row.includes(columnBValue)) {
+            columnIndexB = row.indexOf(columnBValue);
+            console.log(`Значение ${columnBValue} найдено в строке.`);
+          }
+        });
+
+      if (rowIndex !== null && columnIndexB !== null) {
+        // Вставляем новые данные в строку следующую за найденной
+        const newRow = sheet.row(rowIndex + 2);
+
+        // Значения для столбца B
+        newRow.cell(columnIndexB + 1).value(tableData.winner);
+
+        // Значения для столбца C
+        newRow.cell(columnIndexB + 1).value('Побед');
+        newRow.cell(columnIndexB + 2).value('Призовых мест');
+        newRow.cell(columnIndexB + 3).value('Отсутствие соревновательной составляющей');
+
+        // Значения для столбца E
+        newRow.cell(columnIndexB + 5).value(tableData.commandData1);
+        newRow.cell(columnIndexB + 6).value(tableData.commandData2);
+        newRow.cell(columnIndexB + 7).value(tableData.commandData3);
+
+        // Сохраняем изменения в файл
+        await workbook.toFileAsync(filePath);
+
+        console.log('Данные успешно вставлены после строки с значением', columnAValue);
+      } else {
+        console.log(`Значение ${columnAValue} или ${columnBValue} не найдено.`);
+      }
+    } catch (error) {
+      console.error('Ошибка при вставке данных:', error);
+    }
+  }
+
+  // Пример использования функции
+  const filePath = 'example.xlsx';
+  const sheetName = 'ЦОВУ';
+  const columnAValue = '5.4';
+  const columnBValue = tableData.select;
+  console.log(`Значение ${tableData.select} это B`);
+  await insertDataAfterRow(filePath, sheetName, columnAValue, columnBValue);
+});
+
+router.post('/createExcel64', (req, res) => {
   const tableData = req.body;
 
   const workbook = new exceljs.Workbook();
   const sheet = workbook.addWorksheet('Sheet 1');
 
-  sheet.mergeCells('B1', 'B3');
-  sheet.mergeCells('C1', 'D1');
-  sheet.mergeCells('C2', 'D2');
-  sheet.mergeCells('C3', 'D3');
+  sheet.mergeCells('B1', 'D1');
 
   sheet.getCell('A1').value = tableData.select;
 
   sheet.getCell('B1').value = tableData.winner;
 
-  sheet.getCell('C1').value = 'Побед';
-  sheet.getCell('C2').value = 'Призовых мест';
-  sheet.getCell('C3').value = 'Отсутствие соревновательной составляющей';
+  sheet.getCell('E1').value = tableData.commandData1;
+
+  workbook.xlsx
+    .writeFile('table64.xlsx')
+    .then(() => {
+      console.log('File saved successfully');
+      res.send('File saved successfully');
+    })
+    .catch((error) => {
+      console.error('Error saving file:', error);
+      res.status(500).send('Error saving file');
+    });
+});
+
+router.post('/createExcel723', (req, res) => {
+  const tableData = req.body;
+
+  const workbook = new exceljs.Workbook();
+  const sheet = workbook.addWorksheet('Sheet 1');
+
+  sheet.mergeCells('B1', 'B7');
+  sheet.mergeCells('C1', 'C3');
+  sheet.mergeCells('C4', 'C6');
+  sheet.mergeCells('C7', 'D7');
+
+  sheet.getCell('B1').value = tableData.winner;
+
+  sheet.getCell('C1').value = 'Призовые места по итогам командного первенства в номинациях';
+  sheet.getCell('C4').value = 'Призовые места по итогам личного первенства в номинациях';
+  sheet.getCell('C7').value = 'Отсутствие соревновательной составляющей';
+
+  sheet.getCell('D1').value = '1-х мест';
+  sheet.getCell('D2').value = '2-х мест';
+  sheet.getCell('D3').value = '3-х мест';
+  sheet.getCell('D4').value = '1-х мест';
+  sheet.getCell('D5').value = '2-х мест';
+  sheet.getCell('D6').value = '3-х мест';
 
   sheet.getCell('E1').value = tableData.commandData1;
   sheet.getCell('E2').value = tableData.commandData2;
   sheet.getCell('E3').value = tableData.commandData3;
+  sheet.getCell('E4').value = tableData.personalData1;
+  sheet.getCell('E5').value = tableData.personalData2;
+  sheet.getCell('E6').value = tableData.personalData3;
+  sheet.getCell('E7').value = tableData.lackOfCompetitiveComponentData;
 
   workbook.xlsx
-    .writeFile('table54.xlsx')
+    .writeFile('table723.xlsx')
     .then(() => {
       console.log('File saved successfully');
       res.send('File saved successfully');
